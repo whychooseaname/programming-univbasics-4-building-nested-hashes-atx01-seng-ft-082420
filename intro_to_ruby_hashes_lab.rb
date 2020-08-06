b67e1ff7af6226bbf733f73262331e4a6056097f
Edward Acosta <edward.ea.acosta@gmail.com>
2020-08-06 01:30:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -62,7 +62,7 @@ monopoly={
       {
         :reading_railroad=>
         {
-          "mortgage_value"=>100
+          :"mortgage_value"=>100
         },
         :pennsylvania_railroad=> 
         {
