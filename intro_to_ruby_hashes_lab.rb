ee14e2d5d6439fbf29b1ab6970f9b3979d5b9fa3
Edward Acosta <edward.ea.acosta@gmail.com>
2020-08-06 00:57:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,10 +16,10 @@ def monopoly_with_second_tier
   # complexity.
   hash = {
     :railroads => {
-      :pieces => 4  
-      }
+      :pieces => 4,  
       :rent_in_dollars =>{},
       :names => {}
+    }
   }
 end
 
