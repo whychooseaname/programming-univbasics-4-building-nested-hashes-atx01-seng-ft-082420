391e27481f35580bec8d236f44cecf1afcc2c0bd
Edward Acosta <edward.ea.acosta@gmail.com>
2020-08-06 01:20:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -61,9 +61,9 @@ hash = {
       :names=> 
       {
         :reading_railroad=> {"mortgage_value"=>100},
-        :pennsylvania_railroad=> {"mortgage_value"=>"200"},
-        :b_and_o_railroad=> {"mortgage_value"=>"400"},
-        :shortline_railroad=> {"mortgage_value"=>"800"}
+        :pennsylvania_railroad=> {"mortgage_value"=>200},
+        :b_and_o_railroad=> {"mortgage_value"=>400},
+        :shortline_railroad=> {"mortgage_value"=>800}
       }
     }
   }
